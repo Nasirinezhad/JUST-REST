99784eb979c8fa946ccc7c5d73fe337ac9bf0da1
Nasirinezhad <marsav98@gmail.com>
2022-12-08 21:30:34 +0330

    Handle server, error response done!

--- src/Server.php
+++ src/Server.php
@@ -4,89 +4,118 @@ namespace Nasirinezhad\JustRest;
 
 	class Server {
 
-        private $router = null;
-		private $controller = null;
-                
+        protected static $router = null;
+		protected static $request = null;
+		protected static $inctase = null;
 
-		public function addServiceClass($serviceClass) {
-			$this->serviceClasses[] = $serviceClass;
-		}
-	
-		public function handle() {
-			$requestAttributes = $this->getRequestAttributeArray();
+		protected $allowedOrigin = ['*'];
 
-			if ($this->methodIsDefinedInRequest()) {
-				$method = $requestAttributes["method"];
-
-				$serviceClass = $this->getClassContainingMethod($method);
-					
-				if ($serviceClass != null) {
-					$ref = new ReflectionMethod($serviceClass, $method);
-					if (!$ref->isPublic()) {
-						echo json_encode(array('error' => 'API call is invalid.'));
-					        return ;
-					}
-					$params = $ref->getParameters();
-					$paramCount = count($params);
-					$pArray = array();
-					$paramStr = "";
-					
-					$iterator = 0;
-					
-					foreach ($params as $param) {
-						$pArray[strtolower($param->getName())] = null;
-						$paramStr .= $param->getName();
-						if ($iterator != $paramCount-1) {
-							$paramStr .= ", ";
-						}
-						
-						$iterator++;
-					}
-
-					foreach ($pArray as $key => $val) {
-						$pArray[strtolower($key)] = $requestAttributes[strtolower($key)];
-					}
-	
-					if (count($pArray) == $paramCount && !in_array(null, $pArray)) {
-						$result = call_user_func_array(array($serviceClass, $method), $pArray);
-
-						if ($result != null) {
-							echo json_encode($result);
-						}
-					}
-					else {
-						echo json_encode(array('error' => "Required parameter(s) for ". $method .": ". $paramStr));
-					}
-				}
-				else {
-					echo json_encode(array('error' => "The method " . $method . " does not exist."));
-				}
+		public function __construct() {
+			if(self::$inctase == NULL) {
+				self::$inctase = $this;
 			}
-			else {
-				echo json_encode(array('error' => 'No method was requested.'));
+		}
+		
+		public function run()
+		{
+			try {
+				$this->corsHeaders();
+			} catch (\Exception $e) {
+				$this->error($e->getMessage(), 500);
+			}
+			header('Content-Type: application/json; charset=utf-8');
+
+			self::$router = new Router();
+			self::$request = new Request();
+
+			try {
+				$response = Router::getAction()->call();
+			} catch (\Exception $e) {
+				$this->error($e->getMessage(), 400);
+			}
+			if ($response) { 
+				$this->response($response);
 			}
 		}
 
-		private function getClassContainingMethod($method) {
-			$serviceClass = null;
-			foreach ($this->serviceClasses as $class) {
-				if ($this->methodExistsInClass($method, $class)) {
-					$serviceClass = $class;
-				}
+		public function response($obj, $code = 200)
+		{
+			http_response_code($code);
+			echo json_encode($obj);
+		}
+
+		public function error($m, $c = null)
+		{
+			if ($c == null) {
+				$c = $m;
+				$m = $this->errorMessage($c);
 			}
-			return $serviceClass;
+			http_response_code($c);
+			echo json_encode(['error'=> $c, 'message' => $m]);
+		}
+		private function errorMessage($code)
+		{
+			$codes = array(
+				'100' => 'Continue',
+				'200' => 'OK',
+				'201' => 'Created',
+				'202' => 'Accepted',
+				'203' => 'Non-Authoritative Information',
+				'204' => 'No Content',
+				'205' => 'Reset Content',
+				'206' => 'Partial Content',
+				'300' => 'Multiple Choices',
+				'301' => 'Moved Permanently',
+				'302' => 'Found',
+				'303' => 'See Other',
+				'304' => 'Not Modified',
+				'305' => 'Use Proxy',
+				'307' => 'Temporary Redirect',
+				'400' => 'Bad Request',
+				'401' => 'Unauthorized',
+				'402' => 'Payment Required',
+				'403' => 'Forbidden',
+				'404' => 'Not Found',
+				'405' => 'Method Not Allowed',
+				'406' => 'Not Acceptable',
+				'409' => 'Conflict',
+				'410' => 'Gone',
+				'411' => 'Length Required',
+				'412' => 'Precondition Failed',
+				'413' => 'Request Entity Too Large',
+				'414' => 'Request-URI Too Long',
+				'415' => 'Unsupported Media Type',
+				'416' => 'Requested Range Not Satisfiable',
+				'417' => 'Expectation Failed',
+				'500' => 'Internal Server Error',
+				'501' => 'Not Implemented',
+				'503' => 'Service Unavailable'
+			);
+			return $codes["$code"];
+		}
+		public function addOrigin($origin)
+		{
+			if (is_array($origin)) {
+				$this->allowedOrigin = array_merge($this->allowedOrigin, $origin);
+			}elseif (is_string($origin)) {
+				$this->allowedOrigin[] = $origin;
+			}
 		}
 
-		private function methodExistsInClass($method, $class) {
-			return method_exists($class, $method);
-		}
-
-		private function methodIsDefinedInRequest() {
-			return array_key_exists("method", $this->getRequestAttributeArray());
-		}
-
-		private function getRequestAttributeArray() {
-			return array_change_key_case($_REQUEST, CASE_LOWER);;
+		private function corsHeaders() {
+			// to support multiple origins we have to treat origins as an array
+			$allowedOrigin = (array)$this->allowedOrigin;
+			// if no origin header is present then requested origin can be anything (i.e *)
+			$currentOrigin = !empty($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '*';
+			if (in_array($currentOrigin, $allowedOrigin)) {
+				$allowedOrigin = array($currentOrigin); // array ; if there is a match then only one is enough
+			}
+			foreach($allowedOrigin as $allowed_origin) { // to support multiple origins
+				header("Access-Control-Allow-Origin: $allowed_origin");
+			}
+			header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS');
+			header('Access-Control-Allow-Credential: true');
+			header('Access-Control-Allow-Headers: X-Requested-With, content-type, access-control-allow-origin, access-control-allow-methods, access-control-allow-headers, Authorization');
 		}
 	}
 
